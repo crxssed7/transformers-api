af1b6ce63ee8aa1cbc49e919714d98ed70102af1
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-01-09 11:31:07 +0000

    Started allegiance endpoint

--- main.py
+++ main.py
@@ -16,6 +16,11 @@ transformers_put_args.add_argument("first_appearance", type=str, help="The first
 transformers_put_args.add_argument("image", type=str, help="Transformers image", required=False)
 transformers_put_args.add_argument("description", type=str, help="Transformers description", required=False)
 
+allegiance_put_args = reqparse.RequestParser()
+allegiance_put_args.add_argument("name", type=str, help="Name of the allegiance, eg, Autobot", required=True)
+allegiance_put_args.add_argument("alignment", type=str, help="The alignment, eg, Bad", required=True)
+allegiance_put_args.add_argument("image", type=str, help="Logo for the Allegiance", required=False)
+
 transformer_resource_fields = {
     'id': fields.Integer,
     'name': fields.String,
@@ -26,6 +31,13 @@ transformer_resource_fields = {
     'role': fields.String,
     'image': fields.String,
     'description': fields.String
+}
+
+allegiance_resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'alignment': fields.String,
+    'image': fields.String
 }
 
 class TransformerModel(db.Model):
@@ -46,8 +58,6 @@ class AllegianceModel(db.Model):
     alignment = db.Column(db.String(20), nullable=False)
     image = db.Column(db.String(200))
 
-db.create_all()
-
 class Transformer(Resource):
     @marshal_with(transformer_resource_fields)
     def get(self, transformer_id):
@@ -59,21 +69,47 @@ class Transformer(Resource):
     @marshal_with(transformer_resource_fields)
     def put(self, transformer_id):
         args = transformers_put_args.parse_args()
+        # Check if the Transfomer with that ID already exists
         result = TransformerModel.query.filter_by(id=transformer_id).first()
         if result:
             abort(409, message="Transformer with that ID already exists")
+        # Get the allegiance from the allegiance_id, throw an error if not found
         allegiance_result = AllegianceModel.query.filter_by(id=args['allegiance']).first()
         if not allegiance_result:
             abort(404, message="No allegiance found with given ID")
+        # Create a new Transformer instance
         transformer = TransformerModel(id=transformer_id, name=args['name'], allegiance=args['allegiance'], subgroup=args['subgroup'], role=args['role'], image=args['image'], description=args['description'], first_appearance=args['first_appearance'], allegiance_name=allegiance_result.name, subgroup_name="teey")
         db.session.add(transformer)
         db.session.commit()
         return transformer, 201
 
 class Allegiance(Resource):
-    
+    @marshal_with(allegiance_resource_fields)
+    def get(self, allegiance_id):
+        result = AllegianceModel.query.filter_by(id=allegiance_id).first()
+        if not result:
+            abort(404, message="No allegiance was found with that ID")
+        return result
+
+    @marshal_with(allegiance_resource_fields)
+    def put(self, allegiance_id):
+        args = allegiance_put_args.parse_args()
+        # Check that the ID doesn't exist
+        checkForID = AllegianceModel.query.filter_by(id=allegiance_id).first()
+        if checkForID:
+            abort(409, message="Allegiance ID already exists")
+        # Check if the allegiance already exists
+        checkForAllegiance = AllegianceModel.query.filter_by(name=args['name']).first()
+        if checkForAllegiance:
+            abort(409, message="Allegiance already exists with that name")
+        # Create new allegiance instance
+        allegiance = AllegianceModel(id=allegiance_id, name=args['name'], alignment=args['alignment'], image=args['image'])
+        db.session.add(allegiance)
+        db.session.commit()
+        return allegiance, 201
 
 api.add_resource(Transformer, "/transformers/<int:transformer_id>")
+api.add_resource(Allegiance, "/allegiance/<int:allegiance_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,15 +2,24 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
+#data = {
+#    'name': 'Test',
+#    'allegiance': 1,
+#    'subgroup': 1,
+#    'role': 'Role',
+#    'first_appearance': 'yeet',
+#    'image': 'yeet',
+#    'description': 'This is a really long description'
+#}
+#
+#response = requests.get(BASE + "transformers/1")
+#print(response.json())
+
 data = {
-    'name': 'Test',
-    'allegiance': 1,
-    'subgroup': 1,
-    'role': 'Role',
-    'first_appearance': 'yeet',
-    'image': 'yeet',
-    'description': 'This is a really long description'
+    'name': 'Autobot',
+    'alignment': 'Good',
+    'image': 'This is the image url'
 }
 
-response = requests.get(BASE + "transformers/1")
+response = requests.get(BASE + "allegiance/1")
 print(response.json())
