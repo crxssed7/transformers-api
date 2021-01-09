790beb0e35bbd5fffd5334b7ba7df311927fb29d
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-01-09 16:11:25 +0000

    Update test.py

--- test.py
+++ test.py
@@ -37,19 +37,24 @@ BASE = "http://127.0.0.1:5000/"
 #
 #        i = i + 1
 
-with open("transformers.csv", newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    i = 1
-    for row in reader:
-        data = {
-            'name': row[0],
-            'allegiance': row[1],
-            'subgroup': row[2],
-            'role': row[3],
-            'image': row[4]
-        }
+#with open("transformers.csv", newline='') as csvfile:
+#    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+#    i = 1
+#    for row in reader:
+#        data = {
+#            'name': row[0],
+#            'allegiance': row[1],
+#            'subgroup': row[2],
+#            'role': row[3],
+#            'image': row[4]
+#        }
+#
+#        response = requests.put(BASE + "transformers/" + str(i), data=data)
+#        print(response.json())
+#
+#        i = i + 1
 
-        response = requests.put(BASE + "transformers/" + str(i), data=data)
-        print(response.json())
+get_id = input("What ID do you want to search for?")
 
-        i = i + 1
+response = requests.get(BASE + "transformers/" + str(get_id))
+print(response.json())
